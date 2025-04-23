117282657dc46a9a594cf626cbfeaec8501dc79e
Xp0503 <275760490@qq.com>
2025-04-23 17:15:03 +0800

    路由表更改 菜单栏一直出现 默认开始是展示全部商品

--- src/router/index.js
+++ src/router/index.js
@@ -3,45 +3,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ProductPreview from '@/views/products/ProductPreview.vue'
 import ProductDetail from '@/views/products/ProductDetail.vue'
 import ProductCategory from '@/views/products/ProductCategory.vue'
-import CatageryNav from '../components/CatageryNav.vue'
 import LayoutVue from '@/views/Menu.vue'
+
 
 // 路由配置
 const routes = [
     {
-        path: '/',
-        redirect: '/main'
-    },
-    {
-        path: '/main',
-        name: 'Main',
-        component: LayoutVue,
-    },
-    {
-        path: '/products',
-        name: 'ProductPreview',  // 路由名称（用于编程式导航）
-        component: ProductPreview
-    },
-    {
-        path: '/products/:id', // 动态路由参数
-        name: 'ProductDetail',
-        component: ProductDetail,
-        props: true // 将路由参数作为props传递给组件
-    },// 分类商品页
-
-    {
-        path: '/products/category',
-        name: 'CataeryNav',
-        component: CatageryNav,
-
-    },
-    {
-        path: '/products/category/:category',
-        name: 'ProductCategory',
-        component: ProductCategory,
-        props: true
-    },//分类导航
-
+        path: '/', component: LayoutVue, redirect: '/products', children: [
+            { path: '/products', component: ProductPreview },
+            { path: '/products/:id', component: ProductDetail, props: true },// 分类商品页
+            { path: '/products/category/:category', component: ProductCategory, props: true },//分类导航
+        ]
+    }
 ]
 
 // 创建路由实例
